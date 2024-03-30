3359e5d40a701ad5f70070139389c7e2f0ca897f
sergiopamo <sergiopamomamani170605@gmail.com>
2024-03-30 15:40:03 -0500

    primer commit

---
+++ solid_central.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+/*Se desea desarrollar una aplicación de gestión de tareas que permita a los usuarios crear y asignar
+tareas, establecer fechas límite y hacer seguimiento del progreso de las mismas. La aplicación
+debe ser escalable y fácil de mantener, por lo que se desea que se implementen los principios
+SOLID en su diseño. */
+
+int main(){
+    cout<<"Adios mundo.";
+}
